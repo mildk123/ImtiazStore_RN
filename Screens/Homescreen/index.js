79bbfbf851d390681a8c85c8e7c901a613eae2a0
mildk123 <mildk123@gmail.com>
2018-12-11 17:16:35 +0500

    HARDA

--- Screens/Homescreen/index.js
+++ Screens/Homescreen/index.js
@@ -1,17 +1,26 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import React, { Component } from "react";
+import { View, ScrollView, Text } from "react-native";
+import { Header, StatusBar } from "react-native-elements";
 
 class Homescreen extends Component {
-    static navigationOptions = {
-        header: null
-    }
+  static navigationOptions = {
+    header: null
+  };
 
   render() {
     return (
       <View>
-        <Text> Homescreen </Text>
+        <StatusBar backgroundColor="blue" barStyle="light-content" />
+        <Header
+          leftComponent={{ icon: "menu", color: "#fff" }}
+          centerComponent={{ text: "MY TITLE", style: { color: "#fff" } }}
+          rightComponent={{ icon: "search", color: "#fff" }}
+        />
+        <ScrollView>
+          <Text>123123</Text>
+        </ScrollView>
       </View>
-    )
+    );
   }
 }
 
